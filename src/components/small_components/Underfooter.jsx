0ab7cb0fd6d49dafd7d80a1be1c76eb5fea5b438
Paradise96 <dimaparadise333@gmail.com>
2023-08-15 06:24:23 -0700

    final comitt

--- src/components/small_components/Underfooter.jsx
+++ src/components/small_components/Underfooter.jsx
@@ -4,7 +4,7 @@ function Underfooter(){
     return(
         <section className="underfooter">
             <div className="container">
-                <div className="underfooter_stan">© 2021 - Stan’s Assets. All Right Reserved</div>
+                <div className="underfooter_stan">© 2023 - Stan’s Assets. All Right Reserved</div>
                 <div className="underfooter_text">
                     <Link to='https://google.com' target="_blank">Privacy policy</Link>
                     <Link to='https://google.com' target="_blank">Cookies policy</Link>
