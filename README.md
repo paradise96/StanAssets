26bfa1fb376619eba663fa7ba36d30306499de8c
Paradise96 <dimaparadise333@gmail.com>
2023-08-15 06:13:38 -0700

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Kurs project by Oleksandr Kovalenko
-
-Your site is live at [View live](https://akovalenko777.github.io/creative/).
--- src/data/FirstScreenInfo.js
+++
@@ -1,4 +0,0 @@
-export const screenInfo = {
-    header: 'FOCUS ON THE PRODUCT AND LET US DO THE REST!',
-    text: 'We’re an Ukrainian software development company focused on just one thing - offshore software development services. We have 16+ years of experience and a team of 450+ developers working efficiently to deliver unique solutions to start-ups, software development companies, enterprises, and digital agencies to simplify their IT outsourcing experience & reduce time/cost to market.'
-}
